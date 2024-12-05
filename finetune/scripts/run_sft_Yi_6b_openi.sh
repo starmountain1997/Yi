04cf8d09e92aa5dcccf3174a223372aeea01370f
guozr <guozr1997@hotmail.com>
2024-12-05 19:52:12 +0800

    add requirements

--- finetune/scripts/run_sft_Yi_6b_openi.sh
+++ finetune/scripts/run_sft_Yi_6b_openi.sh
@@ -14,8 +14,8 @@ MODEL_NAME="/tmp/pretrainmodel/Yi-1.5-9b-chat"
 deepspeed main.py \
 	--data_path /tmp/code/Yi/finetune/yi_example_dataset \
 	--model_name_or_path $MODEL_NAME \
-	--per_device_train_batch_size 2 \
-	--per_device_eval_batch_size 2 \
+	--per_device_train_batch_size 1 \
+	--per_device_eval_batch_size 1 \
 	--max_seq_len 4096 \
 	--learning_rate 2e-6 \
 	--weight_decay 0. \
